6a9ce8a7e761b1586ba220256556b442d84f31c9
Vlad <pacMakaveli90@gmail.com>
2022-05-23 20:51:08 +0100

    chore: release 1.0.2.171.pre1

--- lib/fluent-icons/version.rb
+++ lib/fluent-icons/version.rb
@@ -1,4 +1,4 @@
 module FluentIcons
-  FLUENT_UI_ICONS_VERSION = '1.1.170'.freeze
-  VERSION = '1.0.2.170'.freeze
+  FLUENT_UI_ICONS_VERSION = '1.1.171.pre1'.freeze
+  VERSION = '1.0.2.171.pre1'.freeze
 end
